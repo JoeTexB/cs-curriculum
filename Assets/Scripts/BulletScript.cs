077cbf48f2f63584b652316af90f65ce337215c0
JoeTexB <jbeaman2641@student.pps.net>
2024-10-04 13:50:33 -0700

    Made bullets disapeer and do damage

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,13 +6,14 @@ using UnityEngine.UIElements;
 public class Bullet : MonoBehaviour
 {
     private int Speed = 10;
+    
 
     public Vector3 targetPosition;
     // Start is called before the first frame update
+  
     void Start()
     {
-        //max distance can travel
-        Vector3.MoveTowards(transform.position, targetPosition, 10 * Time.deltaTime);  
+        
     }
 
     // Update is called once per frame
@@ -20,5 +21,11 @@ public class Bullet : MonoBehaviour
     {
       //move towards player  
       transform.position = Vector3.MoveTowards(transform.position, targetPosition, 5 * Time.deltaTime);
+     
     }
 }
+
+   
+
+    
+
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -15,14 +15,28 @@ public class Health : MonoBehaviour
     {
         if (other.CompareTag("Spikes")) 
         {
+            print("Touched Spike");
             gm.health = gm.health - 1;
             print("you health is: "+gm.health);
+            
             
 
 
 
 
         }    
+        if (other.CompareTag("Bullet")) 
+        {
+            print("Bullet Hit Player");
+            gm.health = gm.health - 1;
+            print("you health is: "+gm.health);
+            Destroy(other.gameObject);
+
+
+
+
+
+        } 
 
     }
 
