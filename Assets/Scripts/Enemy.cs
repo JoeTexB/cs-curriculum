05d966cf043d2da835a219842e0dc906e4493543
JoeTexB <jbeaman2641@student.pps.net>
2024-10-15 13:44:53 -0700

    switched to enum unnstead of state numbers and attempted attack radius

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,38 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public List <Vector3> waypoints;
-    private int state = 1;
+
     private int num;
-    
+   
+    enum states
+    {
+        patrol,
+        chase,
+        attack,
+    }
+    private states state;
+                                                                     
     public Vector3 PlayerTargetPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = states.patrol;
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            state = 2;
+            state = states.chase;
+            
             
             PlayerTargetPosition = other.transform.position;
+           
+            //entering attack radius
+            if (state == states.chase)
+            {
+                state = states.attack;
+                print("attacking");
+            }
         }
         
     }
+
+    //exiting attack radius
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) 
+        {
+            if (state == states.attack)
+            {
+                state = states.chase;
+            }
+            
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if (state == 1)
+        //follow waypoints
+        if (state == states.patrol)
         {
             transform.position = Vector3.MoveTowards(transform.position, waypoints[num], 2 * Time.deltaTime);
         }
 
+        //waypoints change
         if (transform.position == waypoints[num])
         {
             num += 1;
@@ -41,16 +76,25 @@ public class Enemy : MonoBehaviour
                 num = 0;
             }
         }
+        
+        //attack player
+        if (state == states.attack)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PlayerTargetPosition, 2 * Time.deltaTime);
+        }
 
-        if (state == 2)
+        //chase player
+        if (state == states.chase)
         {
             transform.position = Vector3.MoveTowards(transform.position, PlayerTargetPosition, 2 * Time.deltaTime);
             
         }
 
+        //return to patrol
         if (transform.position == PlayerTargetPosition)
         {
-            state = 1;
+            state = states.patrol;
+            
         }
 
     }
